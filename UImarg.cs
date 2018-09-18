96e49d34f2a9d73d448f4a8cb485a8e8d81f5ed6
ACmouse <mingxinggs@hotmail.com>
2018-09-18 16:57:37 +0800

    1232

---
+++ UImarg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImarg : MonoBehaviour {
+    private static UImarg inst;
+    public static UImarg Inst {
+        get {
+            if (inst==null) {
+                inst = new UImarg();
+            }
+              return inst;
+        }
+
+    }
+
+	void Awake () {
+    
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
